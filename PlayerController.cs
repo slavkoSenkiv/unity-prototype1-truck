f47f295d4e225603fe4757fbc431c9d0f9d35ce3
slavkoSenkiv <60449898+slavkoSenkiv@users.noreply.github.com>
2022-01-03 09:26:27 +0200

    upd

--- PlayerController.cs
+++ PlayerController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 5.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,10 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // Move the vehicle forward
+        // transform.Translate(0, 0, 1);
+        // transform.Translate(Vector3.forward);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+
     }
 }
